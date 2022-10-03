391ccb4c039736713a417a1568139a1bf0f58896
Elie <75082918+ElieBouillard@users.noreply.github.com>
2022-10-03 21:33:26 +0200

    [FIX] - Layers

--- Assets/Scripts/Network/Player/PlayerGameIdentity.cs
+++ Assets/Scripts/Network/Player/PlayerGameIdentity.cs
@@ -1,14 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using CMF;
-using DG.Tweening;
-using Steamworks;
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
-using UnityEngine.UI;
 
 public class PlayerGameIdentity : PlayerIdentity
 {
--- Assets/Scripts/Network/Player/PlayerIdentity.cs
+++ Assets/Scripts/Network/Player/PlayerIdentity.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Steamworks;
 using UnityEngine;
 using UnityEngine.Assertions.Must;
@@ -18,6 +19,8 @@ public class PlayerIdentity : MonoBehaviour
     public bool IsLocalPlayer() => _isLocalPlayer;
     #endregion
 
+    [SerializeField] private GameObject _mesh;
+    
     public virtual void Initialize(ushort id, string newName)
     {
         _id = id;
@@ -25,6 +28,30 @@ public class PlayerIdentity : MonoBehaviour
         if (_id == NetworkManager.Instance.GetClient().Id) { _isLocalPlayer = true; }
 
         gameObject.name = newName;
+        
+        for (int i = 0; i < NetworkManager.Instance.GetPlayers().ToList().Count; i++)
+        {
+            if (NetworkManager.Instance.GetPlayers().Keys.ToList()[i] == GetId())
+            {
+                if (i == 0)
+                {
+                    _mesh.layer = LayerMask.NameToLayer("Player");
+                }
+                if (i == 1)
+                {
+                    _mesh.layer = LayerMask.NameToLayer("Player2");
+                }
+                if (i == 2)
+                {
+                    _mesh.layer = LayerMask.NameToLayer("Player3");
+                }
+                if (i == 3)
+                {
+                    _mesh.layer = LayerMask.NameToLayer("Player4");
+                }
+                break;
+            }
+        }
     }
     
     public virtual void Initialize(ushort id, ulong steamId)
